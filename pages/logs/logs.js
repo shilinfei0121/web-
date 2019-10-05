2e0eb3b3713bcb77ddb87857bdc9925cae434cb3
施林飞 <244878765@qq.com>
2019-10-05 19:49:29 +0800

    第四此提交

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -1,31 +1,43 @@
 //logs.js
 const util = require('../../utils/util.js')
-
+const app = getApp();
 Page({
   data: {
     logs: [],
     userInfo: {},
+    curIdx: 0,
+    listInfo: [
+      {
+        text: '首页',
+        imgUrl: './image/text.png',
+        curUrl: './image/texter.png',
+      },
+      {
+        text: '兼职入口',
+        imgUrl: './image/card.png',
+        curUrl: './image/carder.png',
+      },
+      {
+        text: '个人中心',
+        imgUrl: './image/view.png',
+        curUrl: './image/viewer.png',
+      },
+    ]
   },
-  zhuye: function () {
-    wx.navigateTo({
-      url: '../zhu/zhu'
+  Navigation: function (event) {
+    var that = this;
+    app.Navigation(event, that);
+  },
+  // 底部导航
+  chooseImg: function (e) {
+    this.setData({
+      curIdx: e.currentTarget.dataset.current
     })
+    //  console.log(e)
+    //  console.log(this.data.curIdx) 
+
   },
-  wei: function () {
-    wx.navigateTo({
-      url: '../wei/wei'
-    })
-  },
-  shen: function () {
-    wx.navigateTo({
-      url: '../shen/shen'
-    })
-  },
-  lao: function () {
-    wx.navigateTo({
-      url: '../lao/lao'
-    })
-  },
+  
   onLoad: function () {
     this.setData({
       logs: (wx.getStorageSync('logs') || []).map(log => {
